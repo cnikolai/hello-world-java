7f92de9544b0cdbf3062dad8a2a0302c02854722
Cynthia Nikolai <cynthianikolai@gmail.com>
2021-09-27 09:46:43 -0400

    Initial commit

---
+++ src/edu/cnm/deepdive/HelloWorld.java
@@ -0,0 +1,14 @@
+package edu.cnm.deepdive;
+
+public class HelloWorld {
+
+  public static void main(String[] args) {
+    //System.out.println(greet);
+    System.out.println("Hello, World!");
+  }
+
+  private static void greet(String greeting) {
+
+  }
+
+}
